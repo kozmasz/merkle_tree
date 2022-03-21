c7abd4564a26ae122dd033d5e30f24ff65a02767
Kozma Szilveszter <szilveszter.kozma@itron.com>
2022-03-21 09:51:30 +0100

    Merkle tree

--- lib/merkle_tree.ex
+++ lib/merkle_tree.ex
@@ -1,4 +1,4 @@
-defmodule MerkelTree do
+defmodule MerkleTree do
   @moduledoc """
   A hash tree or Merkle tree is a tree
   in which every non-leaf node is labelled with the hash of the labels or values (in case of leaves) of its child nodes.
@@ -7,15 +7,39 @@ defmodule MerkelTree do
   """
 
   @doc """
-  It returns with the correct root hash .
+  It returns with the correct root hash.
 
   ## Examples
 
-      iex> MerkelTree.root()
-      "root_hash"
+      iex> MerkleTree.root("test/fixtures/good_transactions.txt")
+      "4a9e8f146d0283c7c3f442d056fe6c8e483b321cd68158bdf00861f7de5260ae"
 
+      iex> MerkleTree.root("test/fixtures/bad_transactions.txt")
+      "133ea61e7eac37082c0dbceea55dc088ba06a1e9b5764ce9f39fcfa7ae001c19"
   """
-  def root do
-    "root_hash"
+  @spec root(String.t) :: String.t
+  def root(filepath) do
+    File.stream!(filepath)
+    |> Stream.map(&String.trim_trailing/1)
+    |> Enum.to_list
+    |> encode
   end
+
+  # PRIVATE METHODS
+
+  @spec encode(List.t, Integer.t) :: String.t
+  defp encode(_transactions, _number \\ 2)
+  defp encode([root_sha], _number), do: root_sha
+  defp encode(transactions, number) do
+    transactions
+    |> Enum.chunk_every(number)
+    |> Enum.map(
+         fn [tr1, tr2] ->
+           :crypto.hash(:sha256, tr1 <> tr2)
+           |> Base.encode16(case: :lower)
+         end
+       )
+    |> encode(number)
+  end
+
 end
--- test/merkel_tree_test.exs
+++ test/merkel_tree_test.exs
@@ -1,8 +1,16 @@
-defmodule MerkelTreeTest do
+defmodule MerkleTreeTest do
   use ExUnit.Case
-  doctest MerkelTree
+  doctest MerkleTree
+
+  @good_filepath "test/fixtures/good_transactions.txt"
+  @bad_filepath  "test/fixtures/bad_transactions.txt"
+  @root_sha      "4a9e8f146d0283c7c3f442d056fe6c8e483b321cd68158bdf00861f7de5260ae"
 
   test "It returns with the correct root hash" do
-    assert MerkelTree.root() == "root_hash"
+    assert MerkleTree.root(@good_filepath) == @root_sha
+  end
+
+  test "It shouldn't match with the correct root hash" do
+    refute MerkleTree.root(@bad_filepath) == @root_sha
   end
 end
